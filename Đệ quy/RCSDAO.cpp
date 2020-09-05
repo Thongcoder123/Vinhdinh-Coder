b08ac8d70ffb26c2f5d150b6a90f43348e1bcdd3
Hà Văn Hoàng <58208726+hhoangcpascal@users.noreply.github.com>
2020-09-05 21:18:11 +0700

    Add files via upload

---
+++ Đệ quy/RCSDAO.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string S;
+
+int main() {
+    getline(cin, S);
+    reverse(S.begin(), S.end());
+    cout << S;
+    return 0;
+}
+    
