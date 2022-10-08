f8e2fe1995709aeafa06a6378a7abd9108035a7b
Thongcoder123 <100895239+Thongcoder123@users.noreply.github.com>
2022-10-08 22:12:49 +0700

    Update BTKCATU3.cpp

--- Phân tập, Quay lui/BTKCATU3.cpp
+++ Phân tập, Quay lui/BTKCATU3.cpp
@@ -1,72 +1,55 @@
-#include <iostream>
-#include <algorithm>
-#define fname "BTKTRAU4"
-#define llong long long
-#define FOR(i, a, b) for(__typeof(a) i = a; i <= b; ++i)
+/*  vì code cũ của anh hoàng chạy 4.2 giây bị quá time .
+    Nên giờ tui (thongtl123 - http://vinhdinhcoder.net/Coder/Details/thongtl123) sẽ thay bằng code của tui. 
+    Anh em tham khảo nha 😘
+*/
 
+/*
+                   Nguyễn Như Thông - 11A2 - THPT ĐỒNG LỘC
+*/
+#pragma GCC optimize("unroll-loops,02,no-stack-protector")
+#include "bits/stdc++.h"
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define int long long
+#define ull unsigned long long
+#define str string
+#define db double
+#define fl float
+#define all(x) x.begin(),x.end()
+#define st first
+#define nd second
+#define task ""
+#define user "thongtl123"
 using namespace std;
 
+const int N = 45;
+int n,m,ans;
+pair <int, int> a[N];
+vector <pair<int, int>> T[2];
 
-llong P[1100006], P2[1100006];
-int a[43], x[43];
-int n, n1, m1 = 0, m2 = 0;
-
-void enter() {
-    scanf("%d", &n); n1 = (n >> 1);
-    for(int i=1; i<=n; ++i) scanf("%d", &a[i]);
-}
-
-void perfect1() {
-    llong sum = 0, sum1 = 0;
-    FOR(i, 1, n1)
-        if (x[i] == 0) sum += a[i];
-        else sum1 += a[i];
-    P[++m1] = sum1 - sum;
-}
-
-void perfect2() {
-    llong sum = 0, sum1 = 0;
-    FOR(i, n1+1, n)
-        if (x[i] == 0) sum += a[i];
-        else sum1 += a[i];
-    P2[++m2] = sum1 - sum;
-}
-
-void try1(int cnt) {
-    if (cnt == n1 + 1) {
-        perfect1();
+void solve(int i, int v, int w, int base, int maxn){
+    if (i==maxn+1){
+        T[base].push_back({w,v});
         return;
     }
-    FOR(i, 0, 1) x[cnt] = i, try1(cnt+1);
+    if (w+a[i].st<=m) solve(i+1,v,w,base,maxn), solve(i+1,v+a[i].nd,w+a[i].st,base,maxn);
+    else solve(i+1,v,w,base,maxn);
 }
 
-void try2(int cnt) {
-    if (cnt == n + 1) {
-        perfect2();
-        return;
+signed main(){
+    IOS
+    cin >> n >> m;
+    for (int i=1;i<=n;i++) cin >> a[i].st >> a[i].nd;
+    solve(1,0,0,0,n/2);
+    solve(n/2+1,0,0,1,n);
+    sort(T[0].begin(),T[0].end());
+    sort(T[1].begin(),T[1].end());
+    for (int i=1;i<T[1].size();i++) T[1][i].nd = max(T[1][i].nd, T[1][i-1].nd);
+    int itr = T[1].size()-1;
+    for (int i=0;i<T[0].size();i++){
+        while(T[0][i].st + T[1][itr].st>m && itr>=0) itr--;
+        if (itr<0) break;
+        ans = max(ans, T[0][i].nd + T[1][itr].nd);
     }
-    FOR(i, 0, 1) x[cnt] = i, try2(cnt+1);
-}
-
-void find_max() {
-    llong res = 1e16;
-    FOR(i, 1, m1) {
-        int j = lower_bound(P2+1, P2+m2+1, -P[i]) - P2;
-        res = min(res, abs(P2[j] + P[i]));
-        if (j > 1) res = min(res, abs(P2[j-1] + P[i]));
-     }
-     cout << res;
-}
-
-int main() {
-
-    enter();
-    try1(1);
-    try2(n1+1);
-
-    sort(P2+1, P2+m2+1);
-    find_max();
-
-    return 0;
+    cout << ans;
 }
     
